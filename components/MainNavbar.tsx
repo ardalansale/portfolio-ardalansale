836dbce3960a5ff2f6d3ce09a4fb0d1c8e0f9bee
Ardalan Sale <114071686+ardalansale@users.noreply.github.com>
2025-08-12 19:02:04 +0200

    Import font
    Start styling

---
+++ components/MainNavbar.tsx
@@ -0,0 +1,13 @@
+export default function MainNavbar() {
+    return (
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Work</li>
+                <li>Projects</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
