a9c785b716ceac3a5b1a197750024caf22a1a019
Ardalan Sale <114071686+ardalansale@users.noreply.github.com>
2025-09-04 21:14:34 +0200

    Ändrat färg på rubriken på Startsidan

--- app/page.tsx
+++ app/page.tsx
@@ -8,13 +8,15 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <main className="flex flex-col gap-4 max-w-170 w-full p-6">
-      <h1 className='text-4xl font-regular'>Ardalan Sale</h1>
-      <h1 className='text-4xl text-zinc-500'>UX Designer & Web Developer</h1>
-      <div className="space-y-4">
+    <main className='flex flex-col gap-8 max-w-170 w-full p-6'>
+      <div className='flex flex-col gap-2'>
+        <h1 className='text-4xl font-regular text-sky-600'>Ardalan Sale</h1>
+        <h1 className='text-3xl text-cyan-500'>UX Designer & Web Developer</h1>
+      </div>
+      <div className='space-y-4'>
         <p>I&apos;m a UX Designer making the leap into Frontend Development; blending design vision with the ability to bring it to life in code.</p>
         <p>Fascinated by how people think, feel and interact, I shape those insights into digital experiences that guide, inspire, and make every interaction feel intentional. I believe the best digital experiences are born where human understanding meets technical precision; where every design choice is backed by empathy and every line of code has purpose.</p>
-        <p className="text-blue-600">I&apos;m currently studying at Lexicon IT-Proffs in Stockholm and looking for an internship from October 30, 2025 to January 29, 2026.</p>
+        <p>I&apos;m currently studying at Lexicon IT-Proffs in Stockholm and looking for an internship from October 30, 2025 to January 29, 2026.</p>
       </div>
     </main>
   );
